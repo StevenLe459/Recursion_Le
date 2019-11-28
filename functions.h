28ec6222a2b22e78f1c406b7ecec04d2e28f6674
Steven Le <steven.le740@my.tccd.edu>
2019-11-28 21:18:49 +0000

    Finished factorial function

--- functions.h
+++ functions.h
@@ -15,4 +15,13 @@ void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) {
     towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
   }
 }
+
+long factorial(int n) {
+  if (n == 1) {
+    return 1;
+  }
+  else {
+    return n * factorial(n - 1);
+  }
+}
 #endif
