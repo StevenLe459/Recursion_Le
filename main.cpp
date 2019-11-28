ef9e4209e79b93901502f9d39553920c88eee862
Steven Le <steven.le740@my.tccd.edu>
2019-11-28 21:45:29 +0000

    Added definition of recursion as a comment

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "functions.h"
 
+//Recursion is a process in which a function calls itself as a subroutine
+
 int main() {
   std::cout << "The factorial of 10 is " << factorial(10) << "." << std::endl;
   std::cout << "This is how to solve the Tower of Hanoi for 5 disks:" << std::endl;
